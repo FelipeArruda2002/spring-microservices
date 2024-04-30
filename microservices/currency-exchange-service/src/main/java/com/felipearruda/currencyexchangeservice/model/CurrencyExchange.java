f4b9cdec6e710b4dc7d92c0640a6d39ed887f035
FelipeArruda2002 <felipearruda.estudos@gmail.com>
2024-04-30 00:22:08 -0300

    [Currency-exchange] Configurando JPA e dados inicializados

--- microservices/currency-exchange-service/src/main/java/com/felipearruda/currencyexchangeservice/model/CurrencyExchange.java
+++ microservices/currency-exchange-service/src/main/java/com/felipearruda/currencyexchangeservice/model/CurrencyExchange.java
@@ -2,10 +2,21 @@ package com.felipearruda.currencyexchangeservice.model;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class CurrencyExchange {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name = "currency_from")
 	private String from;
+	@Column(name = "currency_to")
 	private String to;
 	private BigDecimal conversionMultiple;
 	private String environment;
