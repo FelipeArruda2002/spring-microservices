242e291a131ece8798dd648e92cfd78eaeb67752
FelipeArruda2002 <felipearruda.estudos@gmail.com>
2024-05-07 23:35:05 -0300

    [Currency-Conversion-Service] Iniciando microsserviço de conversão de moeda

---
+++ microservices/currency-conversion-exchange/src/main/java/com/felipearruda/currencyconversionexchange/CurrencyConversionExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.felipearruda.currencyconversionexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionExchangeApplication.class, args);
+	}
+
+}
---
+++ microservices/currency-conversion-exchange/src/test/java/com/felipearruda/currencyconversionexchange/CurrencyConversionExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipearruda.currencyconversionexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
