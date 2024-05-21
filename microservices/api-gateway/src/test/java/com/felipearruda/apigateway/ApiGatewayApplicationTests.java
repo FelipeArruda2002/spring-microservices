8634f267a6d6f50d9523d0279f7c4fb63c341809
FelipeArruda2002 <felipearruda.estudos@gmail.com>
2024-05-21 20:28:12 -0300

    Configurando a API Gateway

---
+++ microservices/api-gateway/src/test/java/com/felipearruda/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipearruda.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
